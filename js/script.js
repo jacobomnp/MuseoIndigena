01a636d8291fc6a779f1d6104c36e34080a6f3be
jacobomnp <61099297+jacobomnp@users.noreply.github.com>
2022-06-12 17:47:12 -0500

    Primero versión museo indígena

---
+++ js/script.js
@@ -0,0 +1,7 @@
+documen.addEventListener('DOMContentLoaded', ()=>{
+	const elementosCarousel=document.querySelectorAll('.carousel');
+	H.Carousel.init(elementosCarousel, {
+		duration: 150
+	}):
+
+});
