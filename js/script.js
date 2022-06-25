ab17e07d45899ac8a62dbebc7b02cc2ed0a48f66
jacobomnp <61099297+jacobomnp@users.noreply.github.com>
2022-06-25 01:06:22 -0500

    Update script.js
    carrrusel funcionando

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-documen.addEventListener('DOMContentLoaded', ()=>{
-	const elementosCarousel=document.querySelectorAll('.carousel');
-	H.Carousel.init(elementosCarousel, {
-		duration: 150
-	}):
-
+document.addEventListener('DOMContentLoaded', () => {
+  const elementosCarousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(elementosCarousel, {
+    duration: 200,
+    dist: -80,
+    shift: 5,
+    padding: 5,
+    numVisible: 3,
+    indicators: true,
+    noWrap: false
+  });
 });
